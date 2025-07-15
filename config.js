9c66d4e6c7c7685c7856b0c3e73e41b04e5d463d
jsdogan <doganbilir01@gmail.com>
2025-07-15 10:24:47 +0300

    İlk yükleme

---
+++ config.js
@@ -0,0 +1,7 @@
+window.siteConfig = {
+  companyName: "Tamircim Teknoloji Hizmetleri Ltd. Şti.",
+  taxNumber: "1234567890",
+  address: "Mersin, Türkiye",
+  phoneNumber: "+90 534 213 12 11",
+  email: "info@tamircim.com"
+};
