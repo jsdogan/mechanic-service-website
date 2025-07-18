2890d23410ee2a44e44e0fd965d4e07bc51a3824
jsdogan <doganbilir01@gmail.com>
2025-07-18 17:38:05 +0300

    hamburger Icon place is changed

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.siteConfig = {
   companyName: "Tamircim Teknoloji Hizmetleri Ltd. Şti.",
   taxNumber: "1234567890",
   address: "Mersin, Türkiye",
-  phoneNumber: "+90 534 213 12 11",
+  phoneNumber: "+90534 213 12 11",
   email: "info@tamircim.com"
 };
